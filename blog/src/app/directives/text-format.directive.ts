7baa4c4a89dd96e6f7f6a86a9e4eab9a786a1fbf
arekop100 <arekop100@gmail.com>
2020-04-07 00:42:00 +0200

    lab done

--- blog/src/app/directives/text-format.directive.ts
+++ blog/src/app/directives/text-format.directive.ts
@@ -4,7 +4,8 @@ import {Directive, ElementRef, HostListener} from '@angular/core';
   selector: '[textFormat]'
 })
 export class TextFormatDirective {
-  constructor(private el: ElementRef) { }
+  constructor(private el: ElementRef) {
+  }
 
   @HostListener('blur') onBlur() {
     const value = this.el.nativeElement.value;
