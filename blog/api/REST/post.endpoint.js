97deb6bb3eaa34fed85a6581ce7b280765d568a7
arekop100 <arekop100@gmail.com>
2020-05-19 15:51:41 +0200

    lab 10 done

--- blog/api/REST/post.endpoint.js
+++ blog/api/REST/post.endpoint.js
@@ -1,6 +1,7 @@
 'use strict';
 import business from '../business/business.container';
 import applicationException from '../services/applicationException';
+import auth from '../middleware/auth';
 
 const postEndpoint = (router) => {
   router.get('/api/posts', async (request, response, next) => {
@@ -22,8 +23,6 @@ const postEndpoint = (router) => {
     }
   });
 
-
-
   router.post('/api/posts/search', async (request, response, next) => {
     try {
       let result = await business(request).getPostManager().search(request.body);
@@ -33,9 +32,7 @@ const postEndpoint = (router) => {
     }
   });
 
-
-
-  router.post('/api/posts', async (request, response, next) => {
+  router.post('/api/posts', auth, async  (request, response, next) => {
     try {
       let result = await business(request).getPostManager().createNewOrUpdate(request.body);
       response.status(200).send(result);
